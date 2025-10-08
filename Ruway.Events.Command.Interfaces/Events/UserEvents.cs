7f740e8970cb49be51005b00863e7b1f91e494bf
Cristian Villegas <cvillegas@ruway.tech>
2025-10-08 17:10:19 -0500

    feat: Update UserDeletedEvent to include ApplicationCode instead of UserName

--- Ruway.Events.Command.Interfaces/Events/UserEvents.cs
+++ Ruway.Events.Command.Interfaces/Events/UserEvents.cs
@@ -58,7 +58,7 @@ public record UserUpdatedEvent(
 /// </summary>
 public record UserDeletedEvent(
     Guid UserId,
-    string UserName
+    string ApplicationCode
 ) : IDomainEvent, INotification, IRoutableEvent
 {
     public Guid EventId { get; } = Guid.NewGuid();
