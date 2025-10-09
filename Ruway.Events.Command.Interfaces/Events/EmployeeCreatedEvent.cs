858ca50d7043c076d0c10d86d9e77c3e087371f6
Cristian Villegas <cvillegas@ruway.tech>
2025-10-09 15:30:16 -0500

    feat: Add EmployeeStores and update EmployeeCreatedEvent and EmployeeUpdatedEvent with additional fields

--- Ruway.Events.Command.Interfaces/Events/EmployeeCreatedEvent.cs
+++ Ruway.Events.Command.Interfaces/Events/EmployeeCreatedEvent.cs
@@ -2,6 +2,14 @@ using MediatR;
 
 namespace Ruway.Events.Command.Interfaces.Events;
 
+public record EmployeeStores(
+   Guid StoreId,
+   DateTime AssignmentDate
+)
+{
+
+}
+
 /// <summary>
 /// Evento que se publica cuando se crea un empleado
 /// </summary>
@@ -11,14 +19,16 @@ public record EmployeeCreatedEvent(
     string LastName,
     string DocumentNumber,
     string Email,
-    string Phone
+    string Phone,
+    string PersonalEmail,
+    EmployeeStores[] EmployeeStores
 ) : IDomainEvent, INotification, IRoutableEvent
 {
     public Guid EventId { get; } = Guid.NewGuid();
     public DateTime OccurredOn { get; } = DateTime.UtcNow;
     public string EventName => nameof(EmployeeCreatedEvent);
     public int Version => 1;
-    
+
     /// <summary>
     /// Routing key específica para eventos de empleados creados
     /// </summary>
--- Ruway.Events.Command.Interfaces/Events/EmployeeUpdatedEvent.cs
+++ Ruway.Events.Command.Interfaces/Events/EmployeeUpdatedEvent.cs
@@ -2,20 +2,27 @@ using MediatR;
 
 namespace Ruway.Events.Command.Interfaces.Events;
 
+
+
 /// <summary>
 /// Evento que se publica cuando se actualiza un empleado
 /// </summary>
 public record EmployeeUpdatedEvent(
     Guid EmployeeId,
-    string Name,
-    string LastName
+    string FirstName,
+    string LastName,
+    string DocumentNumber,
+    string Email,
+    string Phone,
+    string PersonalEmail,
+    EmployeeStores[] EmployeeStores
 ) : IDomainEvent, INotification, IRoutableEvent
 {
     public Guid EventId { get; } = Guid.NewGuid();
     public DateTime OccurredOn { get; } = DateTime.UtcNow;
     public string EventName => nameof(EmployeeUpdatedEvent);
     public int Version => 1;
-    
+
     /// <summary>
     /// Routing key específica para eventos de empleados actualizados
     /// </summary>
